04bece0715b8cef00dedea4fffd80f28cde007cc
riskalaluna <riskalaluna@gmail.com>
2023-03-27 14:44:37 +0700

    Add project files.

---
+++ exerciseProsedural_176/exerciseProsedural_176.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+double rerata(double mtk, double bing); 
+{ 
+	return (nil mtk + nil bing) %2;
+}
+
+string status;
+	
+{
+	if (nilai mtk + nil bing) % 2 = >= 70; 
+	return "lulus";
+	else
+		return "gagal";
+
+	if (nil mtk > 80);
+	return "lulus";
+	else
+		return "gagal";
+}
+   do {
+		cout << "=======================" << endl;
+		cout << "Pilihan (1/2/3) : "; << endl;
+		cout << "Hasil akhir kedua nilai" << endl;
+		cin nilai akhir;
+	
+}
+	switch (nilai akhir) {
+		case 1
+			cout << "Masukkan nilai mtk = ";
+			cin >> nilau mtk;
+			cout << "Masukkan nilai bing = ";
+			cin >> nilai bing;
+			cout << "Nilai akhir = " << rerata((nil mtk + nil bing) % 2 = >= 70);
+			break;
+
+		case 2
+			cout << "Masukkan nilai mtk = ";
+			cin >> nilai mtk;
+			cout >> "Nilai akhir mtk = " << (nil mtk > 80);
+			break;
+
+		case 3
+			break;
+			default =
+			cout << "Nilai akhir tidak memenuhi" << endl;
+}
+
+	void display() {
+		cout << endl;
+		cout << "=======================" << endl;
+		cout << "Nilai akhir yang lulus" << endl;
+		cout << "Nilai akhir yang tidak lulus" << endl;
+}
+
+	int main() {
+		double nilai mtk, nilai bing
+
+	}
